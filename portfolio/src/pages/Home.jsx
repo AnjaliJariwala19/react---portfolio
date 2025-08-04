ab7981bf4ebc7f34ee9e194c043081277cbe1f0e
AnjaliJariwala19 <anjalijariwala3@gmail.com>
2025-08-05 00:02:38 +0530

    Files Uploaded

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,6 @@
+export const Home = () => {
+    return (
+        <div>
+            <h1 className="text-center text-2xl">HOME</h1>
+        </div>)
+}
---
+++ portfolio/src/pages/NotFound.jsx
@@ -0,0 +1,6 @@
+export const NotFound = () => {
+    return (
+        <div>
+            <h1 className="text-center text-2xl">NOT FOUND</h1>
+        </div>)
+}
